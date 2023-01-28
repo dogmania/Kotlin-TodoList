58572087f0040220bff049629ced725042782467
현수 노트북 <hyeonsu@hyeonsuui-MacBookAir.local>
2023-01-28 15:31:11 +0900

    test

---
+++ app/src/main/java/com/example/todolist/db/TodoEntity.kt
@@ -0,0 +1,13 @@
+package com.example.todolist.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity//어떤 구성요소인지 어노테이션을 써서 알려야한다.
+data class TodoEntity (
+    @PrimaryKey(autoGenerate = true) var id : Int? = null,
+    @ColumnInfo(name = "title") var title : String,
+    @ColumnInfo(name = "importance") var importance : Int
+    )
